f8d1192a4b88981230fc9d37b43436d2bd2765e1
liusongrui <402626224@qq.com>
2019-12-23 15:40:50 +0800

    init

---
+++ consumer-demo/src/main/java/com/imain/consumerdemo/controller/DemoController.java
@@ -0,0 +1,8 @@
+package com.imain.consumerdemo.controller;
+
+/**
+ * author Songrui.Liu
+ * date 2019/12/2315:39
+ */
+public class DemoController {
+}
---
+++ consumer-demo/src/test/java/com/imain/consumerdemo/ConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imain.consumerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
